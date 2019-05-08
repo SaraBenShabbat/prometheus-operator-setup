e06ed520ddd457f3958c616e9721ddc988258cf6
Hermeto Romano <hermeto.romano@gmail.com>
2019-05-08 09:02:02 -0300

    removed incubator repository

--- helm/main.tf
+++ helm/main.tf
@@ -9,11 +9,3 @@ data "helm_repository" "stable" {
   name = "stable"
   url  = "https://kubernetes-charts.storage.googleapis.com"
 }
-
-/**
- * Incubator charts
- */
-data "helm_repository" "incubator" {
-  name = "incubator"
-  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
-}
